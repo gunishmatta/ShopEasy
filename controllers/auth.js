b11fa26c9ae1e1fcb7b307419d82415721a8b9bb
Gunish Matta <gunishmatta@gmail.com>
2020-10-22 15:40:46 +0530

    Added auth token generation and signin controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const { validationResult } = require('express-validator');
+
 const User = require('../models/user')
 
-exports.signup = (req, res) => {
+exports.signup = (req, res) =>  {
+ 
+  const errors = validationResult(req)
+ if(!errors.isEmpty())
+ {
+   return res.status(422).json(
+     {
+       error: errors.array()[0].msg
+     }
+   )
+ }
+ 
+ 
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -18,4 +34,47 @@ exports.signup = (req, res) => {
 }
 exports.signout = (req, res) => {
   res.json({ message: "User signed out" });
+}
+
+exports.signin = (req,res) =>
+{
+ const {email,password} = req.body; //destructuring
+ const errors = validationResult(req)
+ if(!errors.isEmpty())
+ {
+   return res.status(422).json(
+     {
+       error: errors.array()[0].msg
+     }
+   )
+ 
+}
+
+User.findOne({email},(err,user)=>{
+if(err)
+{
+return res.status(400).json({
+  error : "User does not exists"
+})
+}
+if(!user.authenticate(password))
+{
+return res.status(400).json({
+  error : "Email or password does not match"
+})
+}
+//created token
+const token = jwt.sign({_id: user._id},process.env.AUTHSECRET)
+
+//putting token to cookie
+res.cookie("token",token,{expire: new Date()+100})
+
+//return response to frontend
+const {_id,name,email,role} = user;
+return res.json({
+  token,
+  user : {_id,name,email,role}
+})
+
+})
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,25 @@
 var express = require("express");
+const { check } = require('express-validator');
 var router = express.Router();
 const {signout,signup} = require('../controllers/auth');
 
-router.post('/signup',signup);
+router.post('/signup',[check("name").isLength({min:3}).withMessage('Name must be at least 3 chars long'),
+
+check("email").isEmail().withMessage(""),
+
+check("securePassword").isLength({ min: 5 }).withMessage('must be at least 5 chars long').matches(/\d/).withMessage('must contain a number')
+
+],signup);
+
+router.post('/sigin',
+[
+check("email").isEmail().withMessage(""),
+
+check("securePassword").isLength({ min: 5 }).withMessage('must be at least 5 chars long').matches(/\d/).withMessage('must contain a number')
+
+],signup);
+
+
 router.get("/signout",signout);
 
 module.exports = router;
