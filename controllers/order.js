2b2d2a9ca7979f2c8269e025c3efa896899675e1
Gunish Matta <gunishmatta@gmail.com>
2020-11-10 22:48:38 +0530

    Setting up frontend and frontend structure

--- controllers/order.js
+++ controllers/order.js
@@ -53,3 +53,32 @@ exports.getAllOrders = (req,res)=>
         res.json(order);
     })
 }
+
+
+exports.getOrderStatus = (req,res) =>
+{
+    res.json(Order.schema.path("status").enumValues)
+
+}
+exports.updateOrderStatus = (req,res) =>
+{
+    Order.update(
+        {_id:req.body.orderId},
+        {$set:{status:req.body.status}},
+        (err,order)=>
+        {
+            if(err)
+            {
+                return res.status.json(
+                    {
+                        error:"Error updating order status"
+                    }
+                )
+            }
+
+       res.json(order);     
+
+        }
+    )
+    
+}
--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById ,pushOrderinPurchaseList } = require("../controllers/user");
-const {createOrder,getOrderById} = require('../controllers/order');
+const {createOrder,getAllOrders,getOrderById,getOrderStatus,updateOrderStatus} = require('../controllers/order');
 const {updateStock} = require('../controllers/product');
 const { chunk } = require("lodash");
 
@@ -17,5 +17,10 @@ router.post('/order/create/:userId',isSignedIn,isAuthenticated,pushOrderinPurcha
 //read
 router.get('/order/all/:userId',isSignedIn,isAuthenticated,isAdmin,getAllOrders);
 
+
+//order status
+router.get('/order/status/:userId',isSignedIn,isAuthenticated,isAdmin,getOrderStatus);
+
+router.put('/order/:orderId/status/:userId',isSignedIn,isAuthenticated,isAdmin,updateOrderStatus);
 
 module.exports = router;
