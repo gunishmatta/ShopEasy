03d57c1d810988f6f2b20ab2b77bca3edb28aed3
gunishmatta <33680363+gunishmatta@users.noreply.github.com>
2020-12-18 04:39:40 +0000

    Admin API CRUD Calls for Products

--- frontend/src/admin/helper/adminapicall.js
+++ frontend/src/admin/helper/adminapicall.js
@@ -1,6 +1,8 @@
 import { response } from "express";
 import { backendAPI } from "../../backend";
 
+
+//category calls
 export const createCategory = (userId, token, category) => {
     return fetch(`${backendAPI}/category/create/${userId}`, {
         method: "POST",
@@ -12,10 +14,84 @@ export const createCategory = (userId, token, category) => {
         },
 
         body: JSON.stringify(category)
-    
+
     }).then(response => {
         return response.json();
     }).catch(err => console.log(err))
 }
 
+//get all categories
+export const getCategories = () =>
+{
+    return fetch(`${backendAPI}/categories`,
+    {
+        method:"GET"
+    }).then(response => response.json()).catch(err=>console.log(err))
+}
 
+
+//product calls
+export const createProduct = (userId, token, product) => {
+    return fetch(`${backendAPI}/product/create/${userId}`, {
+        method: "POST",
+        headers:
+        {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    }).then(response => {
+        return response.json()
+    }).catch(err => console.log(err))
+}
+
+export const getProducts = () =>
+{
+    return fetch(`${backendAPI}/products`,
+    {
+        method:"GET"
+    }).then(response => response.json()).catch(err=>console.log(err))
+}
+
+//delete a product
+
+export const deleteProduct = (userId, token, productId) => {
+    return fetch(`${backendAPI}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers:
+        {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+    }).then(response => {
+        return response.json()
+    }).catch(err => console.log(err))
+}
+
+
+// get one product
+
+export const getProduct = () =>
+{
+    return fetch(`${backendAPI}/product/${productId}`,{
+        method:"GET"
+    }).then(response => response.json()).catch(err=>console.log(err))
+}
+
+
+//update a product
+export const updateProduct = (productId,userId,token,product) =>
+{
+    return fetch(`${backendAPI}/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers:
+        {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    }).then(response => {
+        return response.json()
+    }).catch(err => console.log(err))
+   
+}
